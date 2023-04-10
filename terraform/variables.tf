215c2d00089606839ea979702556fd29956dd810
Joshua Heslin <josh@cloutly.com>
2023-04-10 11:17:19 +0100

    Remove ds store

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,11 @@ variable "app_name" {
   default = "twitter-poster"
   description = "name of the app"
 }
+
+variable "open_ai_api_key" {
+  description = "key for open ai"
+}
+
+variable "twitter_api_key" {
+  description = "key for twitter"
+}
